fbb0f813cb547549c80f14069a29339a7abe15d0
Xavier Stevens <xavier@simple.com>
2017-02-01 15:21:37 -0800

    Refactoring large parts of gadget state enable into write_to methods on each struct

--- src/function/mod.rs
+++ src/function/mod.rs
@@ -1,13 +1,11 @@
 pub mod hid;
 pub mod ecm;
 
-use std::collections::HashMap;
 use std::io;
 use std::path::Path;
 
 pub trait UsbGadgetFunction {
     fn instance_name(&self) -> &str;
     fn function_type(&self) -> &str;
-    fn attributes(&self) -> HashMap<&str, Vec<u8>>;
-    fn write_to(&self, base_path: &Path) -> io::Result<()>;
+    fn write_to(&self, functions_path: &Path) -> io::Result<()>;
 }
--- src/gadget.rs
+++ src/gadget.rs
@@ -1,5 +1,9 @@
+use std::io;
+use std::path::Path;
+
 use function::UsbGadgetFunction;
 use config::UsbGadgetConfig;
+use util::{create_dir_if_not_exists, write_data};
 
 // USB Language Identifiers (LANGIDs)
 // http://www.usb.org/developers/docs/USB_LANGIDs.pdf
@@ -50,5 +54,54 @@ impl<'a> UsbGadget<'a> {
             functions: Vec::new(),
             configs: Vec::new(),
         }
+    }
+
+    pub fn write_to(&self, gadget_path: &Path) -> io::Result<()> {
+        try!(create_dir_if_not_exists(&gadget_path));
+
+        // vendor and product id
+        try!(write_data(gadget_path.join("idVendor").as_path(),
+                        format!("0x{:04x}", self.vendor_id).as_bytes()));
+        try!(write_data(gadget_path.join("idProduct").as_path(),
+                        format!("0x{:04x}", self.product_id).as_bytes()));
+
+        // bcdDevice and bcdUSB
+        if let Some(bcd_device) = self.bcd_device {
+            try!(write_data(gadget_path.join("bcdDevice").as_path(),
+                            format!("0x{:04x}", bcd_device).as_bytes()));
+        }
+        if let Some(bcd_usb) = self.bcd_usb {
+            try!(write_data(gadget_path.join("bcdUSB").as_path(),
+                            format!("0x{:04x}", bcd_usb).as_bytes()));
+        }
+
+        // string attributes
+        let lang = format!("0x{:04x}", &self.lang);
+        let strings_path = gadget_path.join("strings").join(&lang);
+        try!(create_dir_if_not_exists(&strings_path));
+        try!(write_data(strings_path.join("serialnumber").as_path(),
+                        self.serial_number.as_bytes()));
+        try!(write_data(strings_path.join("manufacturer").as_path(),
+                        self.manufacturer.as_bytes()));
+        try!(write_data(strings_path.join("product").as_path(),
+                        self.product.as_bytes()));
+
+        // functions
+        let functions_path = gadget_path.join("functions");
+        try!(create_dir_if_not_exists(&functions_path));
+        for func in &self.functions {
+            try!(func.write_to(functions_path.as_path()));
+        }
+
+        // configs
+        let configs_path = gadget_path.join("configs");
+        try!(create_dir_if_not_exists(&configs_path));
+        for config in &self.configs {
+            try!(config.write_to(configs_path.as_path(),
+                                 functions_path.as_path(),
+                                 lang.as_str()));
+        }
+
+        Ok(())
     }
 }
